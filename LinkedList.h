993dbea7cd7a3f8f30871c77813277cdcb088b94
jmora258 <jmora258@ucla.edu>
2015-01-25 17:06:01 -0800

    Update LinkedList Class file and include addItem(string v) function prototype.

--- LinkedList.h
+++ LinkedList.h
@@ -4,9 +4,6 @@
 
 // LinkedList.h
 
-
-
-
 struct Node 
 {
 	string value; 
@@ -16,12 +13,13 @@ struct Node
 class LinkedList 
 {
 	public: 
-		LinkedList()  {...};
-		void addToFront(string v) {...};
-		void addToRear(string v)  {...};
-		void deleteItem(string v)  {...};
-		bool findItem(string v) {...};
-		void printItems() {...};
+		LinkedList();
+		void addToFront(string v);
+		void addToRear(string v);
+		void addItem(string v); // add item to anywhere in the list
+		void deleteItem(string v);
+		bool findItem(string v);
+		void printItems();
 	
 	private: 
 	
